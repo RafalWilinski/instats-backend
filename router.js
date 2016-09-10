4506d237d041e3a0e2ec4adf0872b027c075eaa2
Rafal Wilinski <raf.wilinski@gmail.com>
2016-09-11 01:39:08 +0200

    Fix batch route path

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ router.get('/get_followers', UserController.getFollowers);
 router.get('/get_followings', UserController.getFollowings);
 router.get('/get_stats', UserController.getStats);
 router.get('/get_user_info', UserController.getUserInfo);
-router.get('/get_user_info_batch', UserController.getUsersInfo);
+router.get('/get_user_info_batch', UserController.getUserInfoBatch);
 router.post('/request_access_token', UserController.exchangeCodeForToken);
 router.post('/promote', UserController.promoteUser);
 
