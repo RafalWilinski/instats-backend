8ef274e4139f4ab5e5f6fdecea15757013d9ed9e
Rafal Wilinski <raf.wilinski@gmail.com>
2017-04-01 17:38:38 +0200

    Refactor isUserRegistered function

--- controllers/User/index.js
+++ controllers/User/index.js
@@ -1,8 +1,10 @@
+const isUserRegistered = (instagramId) => require('./helpers/isUserRegistered')(instagramId);
+
 module.exports = {
   exchangeCodeForToken: require('./exchangeCodeForToken'),
   getPhotos: require('./photo/getPhotos'),
   getPhotoAnalytics: require('./photo/getPhotoAnalytics'),
-  isUserRegistered: require('./helpers/isUserRegistered'),
+  isUserRegistered,
   registerUser: require('./register'),
   updateAccessToken: require('./helpers/updateAccessToken'),
   getFollowersDelta: require('./getFollowersDelta'),
