0f4eea36c2ed75e099ccded859d505167bd6003b
Rafal Wilinski <raf.wilinski@gmail.com>
2017-04-04 00:05:54 +0200

    Refactor query to JOIN small_profiles

--- controllers/User/getFollowersDelta.js
+++ controllers/User/getFollowersDelta.js
@@ -8,15 +8,14 @@ const getFollowersDelta = (req, res) => {
   }
 
   postgres('followers_deltas')
-    .select('*')
+    .select('timestamp as time', 'in_or_out as flow', 's.name as name', 's.picture_url as pic')
+    .joinRaw('JOIN small_profiles s ON s.instagram_id = ANY(followers_deltas.users_array)')
     .where({
       user_ref: req.params.userId,
     })
     .whereBetween('timestamp', helpers.getBetweenDates(req))
-    .limit(helpers.getLimit(req))
-    .offset(helpers.getOffset(req))
     .then((data) => responses.returnData(data, res))
-    .catch((error) => responses.returnStatus('Internal Database Error', 500, res));
+    .catch((error) => responses.returnStatus('Internal Database Error', 500, res, error));
 };
 
 module.exports = getFollowersDelta;
